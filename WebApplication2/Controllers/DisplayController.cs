69bb7c4d4803486571277f37ca97d0eda72c6a57
Youssef Amr <155576740+Yamr2001@users.noreply.github.com>
2024-02-03 18:16:18 +0200

    Add project files.

---
+++ WebApplication2/Controllers/DisplayController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class DisplayController : Controller
+    {
+        readonly EDA_medical_devicesEntities dbContext = new EDA_medical_devicesEntities();
+        // GET: Display
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetItems(string searchTerm)
+       {
+            var items = dbContext.MDNames
+                .Where(item => item.MDName_Ar.Contains(searchTerm))
+                .Take(20)
+                .Select(item => new { id = item.MDNameIDPK, text = item.MDName_Ar })
+                .ToList();
+
+            return Json(items, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
